6526d192907e54462e626f5b4d00a78e6d809921
LucianoGanzer <lucianoganzero94@gmail.com>
2023-07-17 17:39:45 -0300

    Tarea 3

---
+++ M3T3/src/Coche.java
@@ -0,0 +1,10 @@
+public class Coche {
+    private int puertas=3;
+
+    public void aumentarPuertas(){
+        this.puertas+=1;
+    }
+    public int getPuertas(){
+        return this.puertas;
+    }
+}
