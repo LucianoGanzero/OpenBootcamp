585450e07d8efd9f27d149f9dcf2c58b03fd1308
LucianoGanzer <lucianoganzero94@gmail.com>
2023-07-17 19:54:37 -0300

    Tarea 8

---
+++ Introduccion/Modulo8/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args){
+        Persona persona = new Persona();
+        persona.setEdad(29);
+        persona.setNombre("Luciano");
+        persona.setTelefono("555-5555");
+        System.out.println(persona.getNombre() +" "+ persona.getEdad() +" "+ persona.getTelefono());
+    }
+}
+
+class Persona{
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
