5483678589909b5c7f7c573bbacebc908e142ade
LucianoGanzer <lucianoganzero94@gmail.com>
2023-07-19 12:29:31 -0300

    Modulos Python

---
+++ PythonM/Modulo7/main.py
@@ -0,0 +1,10 @@
+from Modulo7 import Calculador as Calc
+
+
+a = 10
+b = 5
+print(a)
+print("La suma de ", a, "y", b, " es ", Calc.suma(a, b))
+print("La resta de ", a, "y", b, " es ", Calc.resta(a, b))
+print("La multiplicacion de ", a, "y", b, " es ", Calc.multiplicacion(a, b))
+print("La division de ", a, "y", b, " es ", Calc.division(a, b))
