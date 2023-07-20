1a0e0db57c6f07277ab98b443242b747f4369643
LucianoGanzer <lucianoganzero94@gmail.com>
2023-07-20 12:59:57 -0300

    DB

--- PythonM/Modulo11/BD.py
+++ PythonM/Modulo11/BD.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('alumnosDB.db')
 cursor = conn.cursor()
 
-data = cursor.execute('SELECT * FROM alumnos')
+data = cursor.execute('SELECT * FROM alumnos WHERE nombre="Matias"')
 for row in data:
     print(row)
 
