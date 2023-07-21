093736c4f47c581530eb452e5ea1b62877958d48
LucianoGanzer <lucianoganzero94@gmail.com>
2023-07-21 11:34:24 -0300

    Tipos de datos

---
+++ Java/src/Modulo2/Ejercicio.java
@@ -0,0 +1,17 @@
+package Modulo2;
+
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        int numero = 2;
+        System.out.println(numero);
+        boolean booleano = true;
+        System.out.println(booleano);
+        String palabra = "Hola mundo";
+        System.out.println(palabra);
+        long largo = 2345;
+        System.out.println(largo);
+        double decimal = 5.23;
+        System.out.println(decimal);
+    }
+}
